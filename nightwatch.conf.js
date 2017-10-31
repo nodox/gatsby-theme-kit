b04405981f57fa1b0a8e2f68824c954c647bd823
nodox <snatera09@gmail.com>
2017-10-31 06:24:22 -0400

    add travis ci integration
    The feature test suite can not run in the CI. Now we
    can do feature testing on the master branch to ensure
    the end user experience has not broken.

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,5 +1,16 @@
+// Helpful
+// https://gist.github.com/awatson1978/f94c1b7cab7a331d8d18
+
+const jar = require('selenium-server-standalone-jar');
+
 const BINPATH = './node_modules/nightwatch/bin/';
 const SCREENSHOT_PATH = "./screenshots/";
+
+const CHROME_BINARY_PATH = () => {
+  const local_path = "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary";
+  const travis_path = "google-chrome-stable";
+  return process.env === 'travis' ? 'travis_path' : 'local_path';
+}
 
 module.exports = {
   "src_folders" : ["e2e"],
@@ -7,13 +18,10 @@ module.exports = {
 
   "selenium" : {
     "start_process" : true,
-    "server_path" : BINPATH + "selenium.jar",
-
+    "server_path": jar.path,
     "port" : 4444,
     "cli_args" : {
       "webdriver.chrome.driver" : BINPATH + "chromedriver",
-      // "webdriver.gecko.driver" : "",
-      // "webdriver.edge.driver" : ""
     }
   },
 
@@ -39,16 +47,16 @@ module.exports = {
           "args": [
             "--headless",
           ],
-          "binary": "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary"
-
+          "binary": CHROME_BINARY_PATH
         },
         "javascriptEnabled": true,
-        // "acceptSslCerts": true
+        "acceptSslCerts": true
       }
     },
 
   }
 }
+
 /**
  * selenium-download does exactly what it's name suggests;
  * downloads (or updates) the version of Selenium (& chromedriver)
@@ -59,33 +67,8 @@ module.exports = {
 require('fs').stat(BINPATH + 'selenium.jar', function (err, stat) { // got it?
   if (err || !stat || stat.size < 1) {
     require('selenium-download').ensure(BINPATH, function(error) {
-      if (error) throw new Error(error); // no point continuing so exit!
+      if (error) console.log(error); // no point continuing so exit!
       console.log('✔ Selenium & Chromedriver downloaded to:', BINPATH);
     });
   }
 });
-
-// function padLeft (count) { // theregister.co.uk/2016/03/23/npm_left_pad_chaos/
-//   return count < 10 ? '0' + count : count.toString();
-// }
-
-// var FILECOUNT = 0; // "global" screenshot file count
-/**
- * The default is to save screenshots to the root of your project even though
- * there is a screenshots path in the config object above! ... so we need a
- * function that returns the correct path for storing our screenshots.
- * While we're at it, we are adding some meta-data to the filename, specifically
- * the Platform/Browser where the test was run and the test (file) name.
- */
-// function imgpath (browser) {
-//   var a = browser.options.desiredCapabilities;
-//   var meta = [a.platform];
-//   meta.push(a.browserName ? a.browserName : 'any');
-//   meta.push(a.version ? a.version : 'any');
-//   meta.push(a.name); // this is the test filename so always exists.
-//   var metadata = meta.join('~').toLowerCase().replace(/ /g, '');
-//   return SCREENSHOT_PATH + metadata + '_' + padLeft(FILECOUNT++) + '_';
-// }
-//
-// module.exports.imgpath = imgpath;
-module.exports.SCREENSHOT_PATH = SCREENSHOT_PATH;
