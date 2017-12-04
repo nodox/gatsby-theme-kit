03cdca457527da1455632321dd483643ed90be24
nodox <snatera09@gmail.com>
2017-12-04 05:58:19 -0500

    Replace section with Header component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { Header } from '../components/Header';
 
 import '../styles/main.scss';
 
@@ -12,26 +13,8 @@ export default function Index({ data }) {
   			<div id="wrapper">
 
   				{/* <!-- Header --> */}
-  					<header id="header">
-  						<div class="logo">
-  							<span class="icon fa-diamond"></span>
-  						</div>
-  						<div class="content">
-  							<div class="inner">
-  								<h1>Dimension</h1>
-  								<p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-  								for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-  							</div>
-  						</div>
-  						<nav>
-  							<ul>
-  								<li><a href="#intro">Intro</a></li>
-  								<li><a href="#work">Work</a></li>
-  								<li><a href="#about">About</a></li>
-  								<li><a href="#contact">Contact</a></li>
-  							</ul>
-  						</nav>
-  					</header>
+          <Header />
+
 
     				{/* <!-- Main --> */}
   					<div id="main">
