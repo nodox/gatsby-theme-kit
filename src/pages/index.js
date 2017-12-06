b9ef43a5c267bc4d78af184b8ad3bc358faaf449
nodox <snatera09@gmail.com>
2017-12-06 06:57:24 -0500

    setup theme to look like the default
    We need a way for the user to see the elements of the stylesheet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-import { Starter } from '../components/Starter';
+import avatar from '../images/avatar.jpg';
+
+import '../styles/main.scss';
 
 export default function Index({ data }) {
 
+  // document.body.classList.add('is-loading');
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-      <Starter />
-      <Link to="/start">Start</Link>
-    </div>
+			<div id="wrapper">
+
+				{/* <!-- Main --> */}
+					<section id="main">
+						<header>
+							<span className="avatar"><img src={avatar} alt="" /></span>
+							<h1>Jane Doe</h1>
+							<p>Senior Psychonautics Engineer</p>
+						</header>
+
+						<footer>
+							<ul className="icons">
+								<li><a href="#" className="fa-twitter">Twitter</a></li>
+								<li><a href="#" className="fa-instagram">Instagram</a></li>
+								<li><a href="#" className="fa-facebook">Facebook</a></li>
+							</ul>
+						</footer>
+					</section>
+
+				{/* <!-- Footer --> */}
+					<footer id="footer">
+						<ul className="copyright">
+							<li>&copy; Jane Doe</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						</ul>
+					</footer>
+
+			</div>
   );
 }
