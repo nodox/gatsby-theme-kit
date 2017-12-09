07044dd64fc031931d38d1f5f28edcdbf68b4d09
nodox <snatera09@gmail.com>
2017-12-09 08:38:20 -0500

    prev button is working

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,32 +3,32 @@ import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import '../styles/main.scss';
 
-import image_fulls_01 from '../images/fulls/01.jpg';
-import image_fulls_02 from '../images/fulls/02.jpg';
-import image_fulls_03 from '../images/fulls/03.jpg';
-import image_fulls_04 from '../images/fulls/04.jpg';
-import image_fulls_05 from '../images/fulls/05.jpg';
-import image_fulls_06 from '../images/fulls/06.jpg';
-import image_fulls_07 from '../images/fulls/07.jpg';
-import image_fulls_08 from '../images/fulls/08.jpg';
-import image_fulls_09 from '../images/fulls/09.jpg';
-import image_fulls_10 from '../images/fulls/10.jpg';
-import image_fulls_11 from '../images/fulls/11.jpg';
-import image_fulls_12 from '../images/fulls/12.jpg';
+import image_fulls_0 from '../images/fulls/01.jpg';
+import image_fulls_1 from '../images/fulls/02.jpg';
+import image_fulls_2 from '../images/fulls/03.jpg';
+import image_fulls_3 from '../images/fulls/04.jpg';
+import image_fulls_4 from '../images/fulls/05.jpg';
+import image_fulls_5 from '../images/fulls/06.jpg';
+import image_fulls_6 from '../images/fulls/07.jpg';
+import image_fulls_7 from '../images/fulls/08.jpg';
+import image_fulls_8 from '../images/fulls/09.jpg';
+import image_fulls_9 from '../images/fulls/10.jpg';
+import image_fulls_10 from '../images/fulls/11.jpg';
+import image_fulls_11 from '../images/fulls/12.jpg';
 
 
-import image_thumbs_01 from '../images/thumbs/01.jpg';
-import image_thumbs_02 from '../images/thumbs/02.jpg';
-import image_thumbs_03 from '../images/thumbs/03.jpg';
-import image_thumbs_04 from '../images/thumbs/04.jpg';
-import image_thumbs_05 from '../images/thumbs/05.jpg';
-import image_thumbs_06 from '../images/thumbs/06.jpg';
-import image_thumbs_07 from '../images/thumbs/07.jpg';
-import image_thumbs_08 from '../images/thumbs/08.jpg';
-import image_thumbs_09 from '../images/thumbs/09.jpg';
-import image_thumbs_10 from '../images/thumbs/10.jpg';
-import image_thumbs_11 from '../images/thumbs/11.jpg';
-import image_thumbs_12 from '../images/thumbs/12.jpg';
+import image_thumbs_0 from '../images/thumbs/01.jpg';
+import image_thumbs_1 from '../images/thumbs/02.jpg';
+import image_thumbs_2 from '../images/thumbs/03.jpg';
+import image_thumbs_3 from '../images/thumbs/04.jpg';
+import image_thumbs_4 from '../images/thumbs/05.jpg';
+import image_thumbs_5 from '../images/thumbs/06.jpg';
+import image_thumbs_6 from '../images/thumbs/07.jpg';
+import image_thumbs_7 from '../images/thumbs/08.jpg';
+import image_thumbs_8 from '../images/thumbs/09.jpg';
+import image_thumbs_9  from '../images/thumbs/10.jpg';
+import image_thumbs_10 from '../images/thumbs/11.jpg';
+import image_thumbs_11 from '../images/thumbs/12.jpg';
 
 
 
@@ -37,40 +37,77 @@ export default class Index extends React.Component {
   constructor(props) {
     super(props);
 
+    this.thumb_imgs = [
+      image_fulls_0,
+      image_fulls_1,
+      image_fulls_2,
+      image_fulls_3,
+      image_fulls_4,
+      image_fulls_5,
+      image_fulls_6,
+      image_fulls_7,
+      image_fulls_8,
+      image_fulls_9,
+      image_fulls_10,
+      image_fulls_11,
+    ],
+
     this.state = {
-      current: image_thumbs_01,
+      currentImg: image_thumbs_1,
+      currentIdx: 1,
+
     }
   }
 
-
-  setCurrentImage(path) {
+  setCurrentImageAndIdx(path, pos) {
     this.setState({
-      current: path,
+      currentImg: path,
+      currentIdx: pos,
     })
   }
 
+  getNextImage() {
 
+    this.setState((prevState, props) => {
+      var newPos = (prevState.currentIdx + 1) % this.thumb_imgs.length;
+      var newImg = this.thumb_imgs[prevState.currentIdx % this.thumb_imgs.length];
+
+      return {
+        currentImg: newImg,
+        currentIdx: newPos,
+      };
+    });
+  }
+
+  getPrevImage() {
+
+    this.setState((prevState, props) => {
+      var len = this.thumb_imgs.length;
+      var newPos = this.state.currentIdx;
+
+      if (newPos <= 0) {
+        newPos = len - 1;
+      } else {
+        newPos--;
+      }
+
+      var newImg = this.thumb_imgs[newPos];
+
+      return {
+        currentImg: newImg,
+        currentIdx: newPos,
+      };
+    });
+  }
 
   render() {
-    const thumb_imgs = [
-      image_thumbs_01,
-      image_thumbs_02,
-      image_thumbs_03,
-      image_thumbs_04,
-      image_thumbs_05,
-      image_thumbs_06,
-      image_thumbs_07,
-      image_thumbs_08,
-      image_thumbs_09,
-      image_thumbs_10,
-      image_thumbs_11,
-      image_thumbs_12,
-    ];
 
-    const articles = thumb_imgs.map((obj, idx) => {
+    const articles = this.thumb_imgs.map((obj, idx) => {
+      var pos = idx++;
+
       return (
-        <article key={idx}>
-          <a onClick={() => this.setCurrentImage(obj)} className="thumbnail" href='#' ><img src={obj} alt="" /></a>
+        <article key={pos}>
+          <a onClick={() => this.setCurrentImageAndIdx(obj, pos)} className="thumbnail" href='#' ><img src={obj} alt="" /></a>
           <h2>Diam tempus accumsan</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </article>
@@ -82,8 +119,8 @@ export default class Index extends React.Component {
   		<div>
         <div id="viewer">
           <div className="inner">
-            <div className="nav-next"></div>
-            <div className="nav-previous"></div>
+            <div onClick={() => this.getNextImage()} className="nav-next"></div>
+            <div onClick={() => this.getPrevImage()} className="nav-previous"></div>
             <div className="toggle"></div>
           </div>
           <div className="slide active">
@@ -94,7 +131,7 @@ export default class Index extends React.Component {
             <div className="image"
                 style={{
                   backgroundPosition: 'center center',
-                  backgroundImage: `url(${this.state.current})`}}></div>
+                  backgroundImage: `url(${this.state.currentImg})`}}></div>
           </div>
         </div>
 
