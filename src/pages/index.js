f09fa781b76a725a31302a6d41cef2401112ad6e
nodox <snatera09@gmail.com>
2018-02-03 13:32:29 -0500

    [Feat] Update the homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
-import { Starter } from '../components/Starter';
+
+import '../styles/plexer.scss';
 
 export default function Index({ data }) {
-
+  const { edges: posts } = data.allMarkdownRemark;
   return (
     <div>
       <h1>Hello World!</h1>
-      <Starter />
-      <Link to="/start">Start</Link>
+      <Link to="/about">About</Link>
+      {posts
+        .map(({ node: post }) => {
+          return (
+            <Link key={post.id} to={post.frontmatter.path} className="preview">
+              <h3>{post.frontmatter.title}</h3>
+              <p>{post.excerpt}</p>
+            </Link>
+          );
+        })}
     </div>
   );
 }
+
+
+export const pageQuery = graphql`
+  query PlexerIndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
