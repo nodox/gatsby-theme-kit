99026f5dfd3d715a8cee0d06cdc0429e16072a85
nodox <snatera09@gmail.com>
2018-03-03 14:20:06 -0500

    Rename class attribute on tags
    Reactjs uses className for rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,78 +29,78 @@ export default function Index({ data }) {
 						<h1><a href="index.html"><strong>Multiverse</strong> by HTML5 UP</a></h1>
 						<nav>
 							<ul>
-								<li><a href="#footer" class="icon fa-info-circle">About</a></li>
+								<li><a href="#footer" className="icon fa-info-circle">About</a></li>
 							</ul>
 						</nav>
 					</header>
 
 				{/* <!-- Main --> */}
 					<div id="main">
-						<article class="thumb">
-							<a href="images/fulls/01.jpg" class="image"><img src={thumbImage01} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/01.jpg" className="image" style={{ backgroundImage: `url(${thumbImage01})`}}><img src={thumbImage01} alt="" /></a>
 							<h2>Magna feugiat lorem</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/02.jpg" class="image"><img src={thumbImage02} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/02.jpg" className="image"><img src={thumbImage02} alt="" /></a>
 							<h2>Nisl adipiscing</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/03.jpg" class="image"><img src={thumbImage03} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/03.jpg" className="image"><img src={thumbImage03} alt="" /></a>
 							<h2>Tempus aliquam veroeros</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/04.jpg" class="image"><img src={thumbImage04} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/04.jpg" className="image"><img src={thumbImage04} alt="" /></a>
 							<h2>Aliquam ipsum sed dolore</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/05.jpg" class="image"><img src={thumbImage05} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/05.jpg" className="image"><img src={thumbImage05} alt="" /></a>
 							<h2>Cursis aliquam nisl</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/06.jpg" class="image"><img src={thumbImage06} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/06.jpg" className="image"><img src={thumbImage06} alt="" /></a>
 							<h2>Sed consequat phasellus</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/07.jpg" class="image"><img src={thumbImage07} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/07.jpg" className="image"><img src={thumbImage07} alt="" /></a>
 							<h2>Mauris id tellus arcu</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/08.jpg" class="image"><img src={thumbImage08} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/08.jpg" className="image"><img src={thumbImage08} alt="" /></a>
 							<h2>Nunc vehicula id nulla</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/09.jpg" class="image"><img src={thumbImage09} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/09.jpg" className="image"><img src={thumbImage09} alt="" /></a>
 							<h2>Neque et faucibus viverra</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/10.jpg" class="image"><img src={thumbImage10} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/10.jpg" className="image"><img src={thumbImage10} alt="" /></a>
 							<h2>Mattis ante fermentum</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/11.jpg" class="image"><img src={thumbImage11} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/11.jpg" className="image"><img src={thumbImage11} alt="" /></a>
 							<h2>Sed ac elementum arcu</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
-						<article class="thumb">
-							<a href="images/fulls/12.jpg" class="image"><img src={thumbImage12} alt="" /></a>
+						<article className="thumb">
+							<a href="images/fulls/12.jpg" className="image"><img src={thumbImage12} alt="" /></a>
 							<h2>Vehicula id nulla dignissim</h2>
 							<p>Nunc blandit nisi ligula magna sodales lectus elementum non. Integer id venenatis velit.</p>
 						</article>
 					</div>
 
 				{/* <!-- Footer --> */}
-					<footer id="footer" class="panel">
-						<div class="inner split">
+					<footer id="footer" className="panel">
+						<div className="inner split">
 							<div>
 								<section>
 									<h2>Magna feugiat sed adipiscing</h2>
@@ -108,16 +108,16 @@ export default function Index({ data }) {
 								</section>
 								<section>
 									<h2>Follow me on ...</h2>
-									<ul class="icons">
-										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-										<li><a href="#" class="icon fa-github"><span class="label">GitHub</span></a></li>
-										<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-										<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
+									<ul className="icons">
+										<li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+										<li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+										<li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+										<li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+										<li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+										<li><a href="#" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
 									</ul>
 								</section>
-								<p class="copyright">
+								<p className="copyright">
 									&copy; Unttled. Design: <a href="http://html5up.net">HTML5 UP</a>.
 								</p>
 							</div>
@@ -125,17 +125,17 @@ export default function Index({ data }) {
 								<section>
 									<h2>Get in touch</h2>
 									<form method="post" action="#">
-										<div class="field half first">
+										<div className="field half first">
 											<input type="text" name="name" id="name" placeholder="Name" />
 										</div>
-										<div class="field half">
+										<div className="field half">
 											<input type="text" name="email" id="email" placeholder="Email" />
 										</div>
-										<div class="field">
+										<div className="field">
 											<textarea name="message" id="message" rows="4" placeholder="Message"></textarea>
 										</div>
-										<ul class="actions">
-											<li><input type="submit" value="Send" class="special" /></li>
+										<ul className="actions">
+											<li><input type="submit" value="Send" className="special" /></li>
 											<li><input type="reset" value="Reset" /></li>
 										</ul>
 									</form>
