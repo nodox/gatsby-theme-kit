d2b1658d1f6c856994be93a936d7f576246b7135
pbrudny <pbrudny@gmail.com>
2018-03-09 11:53:25 +0100

    use Header component in index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Link from 'gatsby-link';
-import Helmet from 'react-helmet';
 
 export default function Header(props) {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../components/Header'
 
 import '../sass/main.scss';
 
@@ -7,14 +8,7 @@ export default function Index({ data }) {
   return (
 			<div id="wrapper">
 
-				{/* <!-- Header --> */}
-					<header id="header" class="alt">
-						<span class="logo"><img src="images/logo.svg" alt="" /></span>
-						<h1>Stellar</h1>
-						<p>Just another free, fully responsive site template<br />
-						built by <a href="https://twitter.com/ajlkn">@ajlkn</a> for <a href="https://html5up.net">HTML5 UP</a>.</p>
-					</header>
-
+				<Header />
 				{/* <!-- Nav --> */}
 					<nav id="nav">
 						<ul>
